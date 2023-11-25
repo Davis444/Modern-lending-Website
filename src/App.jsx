30ee18e45e67559851ffbf15a65fb377ab8801be
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-26 01:53:35 +0530

    code update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles";
+const App = () => {
+  return (
+    <>
+      <div className="bg-primary w-full overflow-hidden">hello world</div>
+    </>
+  );
+};
+
+export default App;
