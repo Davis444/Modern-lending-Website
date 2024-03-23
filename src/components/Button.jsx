72d038ee2da366a9d13aff99dbd32643dcc76f00
Gurpreet Singh <gpsingh02414@gmail.com>
2024-03-23 23:00:00 +0530

    added new button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
+import styles from "../styles";
+
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <button
+      type="button"
+      className={`py-4 px-12 bg-blue-gradient font-poppins font-medium text-[18px] text-primary font-bold mt-10 rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
